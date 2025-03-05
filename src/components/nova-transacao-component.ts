76ba08327d34e16a0d93e27bb24fceaacf6ce8fb
Bielz--JS <gabrielminduba@gmail.com>
2025-03-05 00:53:41 -0300

    adicionando tratamento de erros

--- src/components/nova-transacao-component.ts
+++ src/components/nova-transacao-component.ts
@@ -6,27 +6,31 @@ import SaldoComponent from "./saldo-component.js";
 const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
 
 elementoFormulario.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (!elementoFormulario.checkValidity()) {
-        alert("Por favor, preencha todos os campos!");
-        return;
+    try {
+        event.preventDefault();
+        if (!elementoFormulario.checkValidity()) {
+            alert("Por favor, preencha todos os campos!");
+            return;
+        }
+
+        const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao") as HTMLSelectElement;
+        const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement;
+        const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement;
+
+        let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
+        let valor: number = inputValor.valueAsNumber;
+        let data: Date = new Date(inputData.value);
+
+        const novaTransacao: Transacao = {
+            tipoTransacao: tipoTransacao,
+            valor: valor,
+            data: data,
+        };
+
+        Conta.registrarTransacao(novaTransacao);
+        SaldoComponent.atualizar();
+        elementoFormulario.reset();
+    } catch (error) {
+        alert(error.message)
     }
-
-    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao") as HTMLSelectElement;
-    const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement;
-    const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement;
-
-    let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
-    let valor: number = inputValor.valueAsNumber;
-    let data: Date = new Date(inputData.value);
-
-    const novaTransacao: Transacao = {
-        tipoTransacao: tipoTransacao,
-        valor: valor,
-        data: data,
-    };
-
-    Conta.registrarTransacao(novaTransacao);
-    SaldoComponent.atualizar();
-    elementoFormulario.reset();
 });
